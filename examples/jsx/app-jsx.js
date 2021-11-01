6afcdabf787eac88ba862d460e9ff22024d86688
Víctor García <gc.victor@gmail.com>
2021-11-01 19:53:06 +0100

    fix: h-shim

--- examples/jsx/app-jsx.js
+++ examples/jsx/app-jsx.js
@@ -1,6 +1,9 @@
-import {h} from "../src/h.js";
-import {app} from "../src/app.js";
-import {component} from "../src/component.js";
+import { h as hh } from "../src/h.js";
+function h(type, props, children) {
+  return hh(type, props, arguments.length > 3 ? [].slice.call(arguments, 2) : children);
+}
+import { app } from "../src/app.js";
+import { component } from "../src/component.js";
 const App = component(() => {
   return /* @__PURE__ */ h("h1", {
     className: "bold"
--- examples/jsx/h-shim.js
+++ examples/jsx/h-shim.js
@@ -1,2 +1,5 @@
-import { h } from '../src/h.js';
-export { h };
+import { h as hh } from '../src/h.js';
+
+export function h(type, props, children) {
+    return hh(type, props, arguments.length > 3 ? [].slice.call(arguments, 2) : children);
+}
